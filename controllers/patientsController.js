fe7df5874bae7abe207c112e96ac935997ea4c96
JamesKlouda <gusklouja@student.gu.se>
2023-11-23 23:35:01 +0100

    #3 (ongoing) Add patient post function

--- controllers/patientsController.js
+++ controllers/patientsController.js
@@ -27,6 +27,37 @@ async function getPatient(req, res, next) {
     }
 }
 
+async function createPatient(req, res, next) {
+    if (!client.connected) {
+        return res.status(502).json({error: "MQTT client not connected"})
+    }
+
+    const uuid = uuidv4();
+    try {
+        const name = req.body.name;
+        const ssn = req.body.ssn;
+        const email = req.body.email;
+        const publishTopic = "grp20/req/patients/create";
+
+        responseMap.set(uuid, res);
+        client.publish(publishTopic, JSON.stringify({
+            name: name,
+            ssn: ssn,
+            email: email,
+            requestID: uuid
+        }), (err) => {
+            if (err) {
+                next(err)
+            }
+        });
+        await mqttTimeout(uuid, 10000)
+    } catch (err) {
+        responseMap.delete(uuid);
+        next(err);
+    }
+}
+
 module.exports = {
-    getPatient
+    getPatient,
+    createPatient
 };
