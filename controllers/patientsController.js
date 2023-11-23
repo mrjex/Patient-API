19e2842bd781ddba154941767aabbe4f4cbaa1ab
JamesKlouda <gusklouja@student.gu.se>
2023-11-23 23:40:52 +0100

    #3 (ongoing) add patch patient function
    The patch function adds variables to the payload array if they can be found in the request body. The array with variables to be updated are then send togther with the requestID in the payload.

--- controllers/patientsController.js
+++ controllers/patientsController.js
@@ -57,7 +57,47 @@ async function createPatient(req, res, next) {
     }
 }
 
+async function updatePatient(req, res, next) {
+    if (!client.connected) {
+        return res.status(502).json({error: "MQTT client not connected"})
+    }
+    const ssn = req.body.ssn;
+    const email = req.body.email;
+    const name = req.body.name;
+
+    const uuid = uuidv4();
+    try {
+        let payload = []
+        if (name) {
+            payload.push({name: name})
+        }
+        if (ssn) {
+            payload.push({ssn: ssn})
+        }
+        if (email) {
+            payload.push({email: email})
+        }
+
+        const publishTopic = "grp20/req/patients/update";
+
+        responseMap.set(uuid, res);
+        client.publish(publishTopic, JSON.stringify({
+            payload: payload,
+            requestID: uuid
+        }), (err) => {
+            if (err) {
+                next(err)
+            }
+        });
+        await mqttTimeout(uuid, 10000)
+    } catch (err) {
+        responseMap.delete(uuid);
+        next(err);
+    }
+}
+
 module.exports = {
     getPatient,
-    createPatient
+    createPatient,
+    updatePatient
 };
