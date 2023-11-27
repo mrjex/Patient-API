1440aa0bf4a7696695bc57237d104adffc78429b
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 00:43:37 +0100

    #4 (ongoing) Add dentist router

---
+++ routes/dentists.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {getDentists, getDentist} = require('../controllers/dentistController');
+
+// GET dentists all dentists or a subset of dentists
+router.get('/get', getDentists);
+
+// GET dentist with dentistID
+router.get('/get/:dentistsID', getDentist);
