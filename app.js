204e4e3a9d7bcc19dd13c05c0f869ceb6001a354
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:07:37 +0100

    #4 (ongoing) Use dentist router in app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var appointmentsRouter = require('./routes/appointments');
 var timeslotsRouter = require('./routes/timeslots')
+var dentistsRouter = require('./routes/dentists')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/appointments', appointmentsRouter);
 app.use('/timeslots', timeslotsRouter)
+app.use('/dentists', dentistsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
