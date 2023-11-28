1470586319ffd07336b266a73a853d9b0e441699
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:09:46 +0100

    #4 (ongoing) Add missing router export

--- routes/dentists.js
+++ routes/dentists.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getDentists, getDentist} = require('../controllers/dentistController');
+const { getDentists, getDentist } = require('../controllers/dentistController');
 
 // GET dentists all dentists or a subset of dentists
 router.get('/get', getDentists);
 
 // GET dentist with dentistID
 router.get('/get/:dentistsID', getDentist);
+
+module.exports = router;
