c8c2745f9189a5f64fa2811a05d06c849985a86e
Bomanjo <gusbomjod@student.gu.se>
2023-12-05 18:57:00 +0100

    #11 (completed) Add .env to project

--- mqttUtils/MQTTclient.js
+++ mqttUtils/MQTTclient.js
@@ -3,11 +3,11 @@ const { messageHandlers } = require('./messageHandlers');
 
 
 const mqttOptions = {
-    host: 'placeholder',
-    port: 'placeholder',
-    protocol: 'placeholder',
-    username: 'placeholder',
-    password: 'placeholder'
+    host: process.env.MQTT_HOST,
+    port: process.env.MQTT_PORT,
+    protocol: process.env.MQTT_PROTOCOL,
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
 };
 const client = mqtt.connect(mqttOptions);
 
