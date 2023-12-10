b14becdb5e3d38d1224d1e741b52cb7091a4dc56
Bomanjo <gusbomjod@student.gu.se>
2023-12-10 02:47:18 +0100

    #14 (ongoing) make use of authentication for appointment endpoints

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { getUsersAppointments, createAppointment, cancelAppointment } = require('../controllers/appointmentController');
+const { authenticateJWT } = require('../authentication/authentication');
+
 
 /* GET appointments with matching patientID. */
-router.get('/users/:patientID', getUsersAppointments);
+router.get('/users/:patientID', authenticateJWT, getUsersAppointments);
 
 /* POST appointment using a patientID and appointmentID*/
-router.post('/', createAppointment);
+router.post('/', authenticateJWT, createAppointment);
 
 /* DELETE appointment using a appointmentID*/
-router.delete('/:appointmentID', cancelAppointment);
+router.delete('/:appointmentID', authenticateJWT, cancelAppointment);
 
 module.exports = router;
