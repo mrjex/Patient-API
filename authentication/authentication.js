b3817339ca8d8ff293f29103d55167dcf301b857
Bomanjo <gusbomjod@student.gu.se>
2023-12-10 02:46:47 +0100

    #14 (ongoing) change structure of tokenized patient object in authentication.js

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -8,7 +8,7 @@ Authentication inspired by https://www.digitalocean.com/community/tutorials/node
 This function generates a JWT from TOKEN_SECRET in .env, and sets an expiry date.
 The token contains the patient_id*/
 function generateJWT(patient_id) {
-    return jwt.sign({patient_id: patient_id}, process.env.TOKEN_SECRET, {expiresIn: '1800s'})
+    return jwt.sign({patient: {patient_id: patient_id}}, process.env.TOKEN_SECRET, {expiresIn: '1800s'})
 }
 /* This function can be used as a express middleware, it verifies token present in Authorization header
 and attaches the contained patient to the req object.
