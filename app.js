84e4dfc9d22e48c51c34b3f22307ce4e5d2357b6
JamesKlouda <gusklouja@student.gu.se>
2023-12-12 13:12:41 +0100

    #21 (completed) Add subscriber routes to app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var availableTimes = require('./routes/availableTimes')
 var dentistsRouter = require('./routes/dentists')
 var patientsRouter = require('./routes/patients')
 var clinicsRouter = require('./routes/clinics')
+var subscriberRouter = require('./routes/emailSubscriber')
 
 var app = express();
 const cors = require('cors');
@@ -32,6 +33,7 @@ app.use('/availableTimes', availableTimes)
 app.use('/dentists', dentistsRouter)
 app.use('/patients', patientsRouter)
 app.use('/clinics', clinicsRouter)
+app.use('/subscriber', subscriberRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
