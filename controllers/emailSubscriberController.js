04f4a45cc926d9c7300277a31c993f92028d465a
JamesKlouda <gusklouja@student.gu.se>
2023-12-12 13:10:19 +0100

    #21 (ongoing) Add create, get and delete subscriber methods

---
+++ controllers/emailSubscriberController.js
@@ -0,0 +1,99 @@
+const { v4: uuidv4 } = require('uuid');
+const { client } = require("../mqttUtils/MQTTclient");
+const { responseMap } = require('../mqttUtils/responseHandler')
+const { mqttTimeout } = require('../mqttUtils/requestUtils')
+
+//This controller will handle all interactions pertaining to fetching clinics
+
+//Get a subscriber
+async function getSubscriberByID(req, res, next) {
+
+    if (!client.connected){
+        return res.status(502).json({error: "MQTT client not connected"})
+    }
+
+    const uuid = uuidv4();
+
+    try {
+        const patient_id = req.patient.patient_id
+        const publishTopic = "grp20/req/subscriber/get";
+        responseMap.set(uuid, res);
+        client.publish(publishTopic, JSON.stringify({
+            requestID: uuid,
+            patient_id: patient_id
+        }), (err) => {
+            if (err) {
+                next(err)
+            }
+        });
+        await mqttTimeout(uuid, 1000000)
+    } catch (err) {
+        responseMap.delete(uuid);
+        next(err);
+    }
+}
+
+//Create a subscriber
+async function createSubscriber(req, res, next) {
+
+    if (!client.connected){
+        return res.status(502).json({error: "MQTT client not connected"})
+    }
+
+    const uuid = uuidv4();
+
+    try {
+        const patient_id = req.patient.patient_id;
+        const email = req.body.email
+        const clinic = req.body.clinic;
+        const publishTopic = "grp20/req/notification/sub"
+
+        responseMap.set(uuid, res);
+        client.publish(publishTopic, JSON.stringify({
+            requestID: uuid,
+            patient_id: patient_id,
+            email: email,
+            clinic: clinic
+        }), (err) => {
+            if (err) {
+                next(err)
+            }
+        });
+        await mqttTimeout(uuid, 1000000)
+    } catch (err) {
+        responseMap.delete(uuid);
+        next(err);
+    }
+}
+
+//Delete a subscriber
+async function deleteSubscriber(req, res, next) {
+
+    if (!client.connected){
+        return res.status(502).json({error: "MQTT client not connected"})
+    }
+
+    const uuid = uuidv4();
+
+    try {
+        const patient_id = req.patient.patient_id
+        const publishTopic = "grp20/req/notification/unsub";
+        responseMap.set(uuid, res);
+        client.publish(publishTopic, JSON.stringify({
+            requestID: uuid,
+            patient_id: patient_id
+        }), (err) => {
+            if (err) {
+                next(err)
+            }
+        });
+        await mqttTimeout(uuid, 1000000)
+    } catch (err) {
+        responseMap.delete(uuid);
+        next(err);
+    }
+}
+
+module.exports = {
+    getSubscriberByID, createSubscriber, deleteSubscriber
+}
